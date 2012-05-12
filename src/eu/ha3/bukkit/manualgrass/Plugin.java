801aef991f92ede8c5a083a1727078b51c12a46d
Hurricaaane <git@ha3.eu>
2012-05-12 06:03:43 +0200

    Commented the algorithm

--- src/eu/ha3/bukkit/manualgrass/Plugin.java
+++ src/eu/ha3/bukkit/manualgrass/Plugin.java
@@ -54,11 +54,28 @@ public class Plugin extends JavaPlugin implements Listener
 			diameter = radius * 2;
 			radius_squared = radius * radius;
 			
+			/**
+			 * The stencil is [x][y].<br>
+			 * The tables are is [x][y][z]. Not like Minecraft. Remeber this.
+			 * 
+			 */
+			
+			/**
+			 * The diameter and height splice will be +1 longer to ensure odd
+			 * values in calculations.
+			 * 
+			 */
 			stencil = new boolean[diameter + 1][diameter + 1];
 			grass = new boolean[diameter + 1][diameter + 1][1 + height_span * 2];
 			dirt = new boolean[diameter + 1][diameter + 1][1 + height_span * 2];
 			apply = new boolean[diameter + 1][diameter + 1][1 + height_span * 2];
 			
+			/**
+			 * Create a 2D stencil, which will be used as a cylinder with an
+			 * axis on the Minecraft-Y axis, which is the axis leading to the
+			 * sky.
+			 * 
+			 */
 			for (int i = -radius; i <= radius; i++)
 			{
 				for (int j = -radius; j <= radius; j++)
@@ -86,9 +103,11 @@ public class Plugin extends JavaPlugin implements Listener
 	@EventHandler(priority = EventPriority.HIGH)
 	public void playerInteractEvent(PlayerInteractEvent event)
 	{
+		// Check if the config values ware valid.
 		if (!canBeUsed)
 			return;
 		
+		// Looking for the action trigger.
 		if (event.getAction() != Action.RIGHT_CLICK_BLOCK)
 			return;
 		
@@ -100,12 +119,15 @@ public class Plugin extends JavaPlugin implements Listener
 		if (ply.getItemInHand().getTypeId() != itemID)
 			return;
 		
+		// Check for permission.
 		if (!ply.hasPermission("manualgrassspread.simple"))
 			return;
 		
+		// Check for block.
 		if (event.getClickedBlock() == null)
 			return;
 		
+		// Start computing now.
 		World world = ply.getWorld();
 		int xs = event.getClickedBlock().getX();
 		int ys = event.getClickedBlock().getY();
@@ -114,11 +136,25 @@ public class Plugin extends JavaPlugin implements Listener
 		boolean hasGrass = false;
 		boolean hasDirt = false;
 		
-		// FIND
+		/**
+		 * Find grass and dirt in the vicinity.
+		 * 
+		 * We create two stencils: The dirt matrix made of the dirt which have a
+		 * top surface exposed to skylight. The grass matrix made of the grass
+		 * which have a top surface exposed to skylight.
+		 * 
+		 */
 		for (int i = 0; i <= diameter; i++)
 		{
 			for (int j = 0; j <= diameter; j++)
 			{
+				/**
+				 * Only take in account blocks within the stencil. We don't care
+				 * if there are grass blocks outside the stencil that could
+				 * spread towards the dirt in the stencil, even if we should. It
+				 * simplifies the calculation process.
+				 * 
+				 */
 				if (stencil[i][j])
 				{
 					for (int hhh = -height_span; hhh <= height_span; hhh++)
@@ -127,8 +163,25 @@ public class Plugin extends JavaPlugin implements Listener
 						int y = ys + hhh;
 						int z = zs + j - radius;
 						
+						/**
+						 * In order to only paint the top layer of dirt with
+						 * grass, and only paing the dirt with valid grass, we
+						 * filter the top layer.<br>
+						 * We only take in account blocks (dirt and grass) that
+						 * receive light from the sky. The reason why we do this
+						 * is to perform a very very simple check on whether the
+						 * block has something translucent above it. Grass can
+						 * spread in the shadow of other objects as long as
+						 * there is sky light.<br>
+						 * <br>
+						 * This also includes blocks that are underwater. The
+						 * code below has been commented out because it's easier
+						 * to paint water springs this way. Could eventually
+						 * make a console command for that.
+						 */
 						Block block = world.getBlockAt(x, y, z);
-						if (block.getRelative(BlockFace.UP).getLightFromSky() > 0)
+						//Block blockAbove = block.getRelative(BlockFace.UP);
+						if ((block.getRelative(BlockFace.UP).getLightFromSky() > 0) /*&& !block.getRelative(BlockFace.UP).isLiquid()*/)
 						{
 							if (block.getType() == Material.GRASS)
 							{
@@ -161,6 +214,13 @@ public class Plugin extends JavaPlugin implements Listener
 						}
 						else
 						{
+							/**
+							 * Remember to false everything else: We are using
+							 * the table data to create a stencil, and we don't
+							 * want previous values to stay from older
+							 * computation.
+							 * 
+							 */
 							grass[i][j][hhh + height_span] = false;
 							dirt[i][j][hhh + height_span] = false;
 							
@@ -174,19 +234,34 @@ public class Plugin extends JavaPlugin implements Listener
 			
 		}
 		
+		// If there is grass and dirt, begin spread.
 		if (hasGrass && hasDirt)
 		{
-			// ARTIFICIALLY SPREAD
+			/**
+			 * In this algorithm, we dilate the grass matrix into the apply
+			 * matrix.<br>
+			 * <br>
+			 * Explanation: A dirt block transforms into grass if there is at
+			 * least one grass block in proximity of its 26 neighbors, so we
+			 * just have to blindly dilate the grass matrix, and then use the
+			 * dirt matrix as a stencil.
+			 */
 			for (int i = 0; i <= diameter; i++)
 			{
 				for (int j = 0; j <= diameter; j++)
 				{
+					/**
+					 * Only take in account blocks within the stencil.
+					 */
 					if (stencil[i][j])
 					{
 						for (int hhh = -height_span; hhh <= height_span; hhh++)
 						{
 							int hhh_matrix = hhh + height_span;
 							
+							/**
+							 * Check the proximity (dilate).
+							 */
 							boolean shouldSpread = false;
 							for (int ip = -1; !shouldSpread && (ip <= 1); ip++)
 							{
@@ -209,6 +284,9 @@ public class Plugin extends JavaPlugin implements Listener
 									}
 							}
 							
+							/**
+							 * Push result to apply matrix.
+							 */
 							apply[i][j][hhh_matrix] = shouldSpread;
 							
 						}
@@ -219,11 +297,17 @@ public class Plugin extends JavaPlugin implements Listener
 				
 			}
 			
-			// ARTIFICIALLY SPREAD
+			// Actually spread it in the world
 			for (int i = 0; i <= diameter; i++)
 			{
 				for (int j = 0; j <= diameter; j++)
 				{
+					/**
+					 * Only take in account blocks within the stencil. Note that
+					 * this is a but redundant since the code below will never
+					 * be true if we're out of the stencil. It just saves some
+					 * very very little time.
+					 */
 					if (stencil[i][j])
 					{
 						for (int hhh = -height_span; hhh <= height_span; hhh++)
@@ -232,6 +316,14 @@ public class Plugin extends JavaPlugin implements Listener
 							int y = ys + hhh;
 							int z = zs + j - radius;
 							
+							/**
+							 * We check if there is dirt in that position, and
+							 * check if we have to apply the change using the
+							 * apply matrix. The apply matrix is an dilate
+							 * version of the grass matrix, so we use the dirt
+							 * matrix as a stencil.
+							 * 
+							 */
 							if (dirt[i][j][hhh + height_span]
 									&& apply[i][j][hhh + height_span])
 							{
@@ -333,13 +425,18 @@ public class Plugin extends JavaPlugin implements Listener
 		 */
 		
 		Block block = world.getBlockAt(x, y, z);
+		
+		// Record the old state, then set the new type on the block.
+		// TODO: Is oldState persistent when setType is called on block?
 		BlockState oldState = block.getState();
 		block.setType(Material.GRASS);
 		
+		// Send a place event.
 		BlockPlaceEvent placeEvent = new BlockPlaceEvent(block, oldState, block
 				.getRelative(BlockFace.UP), ply.getItemInHand(), ply, true);
 		this.getServer().getPluginManager().callEvent(placeEvent);
 		
+		// If the event is cancelled, revert the change.
 		if (placeEvent.isCancelled() || !placeEvent.canBuild())
 		{
 			block.setTypeIdAndData(oldState.getTypeId(), oldState.getRawData(),
